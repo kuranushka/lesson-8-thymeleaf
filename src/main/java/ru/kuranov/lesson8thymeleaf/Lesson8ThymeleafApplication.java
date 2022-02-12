415ebd937f828530264975bee232309f271a5055
HOME-PC <kuranov.andrey@gmail.com>
2022-02-12 18:49:45 +0300

    Initial commit

---
+++ src/main/java/ru/kuranov/lesson8thymeleaf/Lesson8ThymeleafApplication.java
@@ -0,0 +1,13 @@
+package ru.kuranov.lesson8thymeleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lesson8ThymeleafApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lesson8ThymeleafApplication.class, args);
+    }
+
+}
